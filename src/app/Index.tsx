fc0a2a1a4170f759b96864446e540351202a5943
Noman107c <se22f-107@ssuet.edu.pk>
2025-11-14 20:09:35 +0500

    fix live chats

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -42,14 +42,14 @@ const Index: React.FC = () => {
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.innerHTML = `
-      window.$crisp = [];
-      window.CRISP_WEBSITE_ID = "fd92126f-e479-46d6-a020-a53b58778937";
-      (function() {
-        d = document;
-        s = d.createElement("script");
-        s.src = "https://client.crisp.chat/l.js";
-        s.async = 1;
-        d.getElementsByTagName("head")[0].appendChild(s);
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/69174577f0cd89195c96c419/1ja1eemnc';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
       })();
     `;
     document.head.appendChild(script);
